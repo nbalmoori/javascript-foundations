c4f7a4288043c26b62c7691d35c58b8de1178621
Nikhila Balmoori <nbalmoori@gmail.com>
2022-02-08 23:53:16 -0700

    Complete unicorn test

--- mythical-creatures/exercises/unicorn.js
+++ mythical-creatures/exercises/unicorn.js
@@ -1,8 +1,25 @@
 class Unicorn {
   constructor(name, color) {
     this.name = name;
-    this.color = color;
+    if (color === undefined){
+      this.color = "white";
+    } else {
+    this.color = color}
+    }
+  isWhite() {
+    if(this.color !== "white") {
+      return false;
+    }
+  }
+  says(says) {
+    return `**;* ${says} *;**`
   }
 }
+
+var unicorn = new Unicorn('Louisa');
+
+console.log(unicorn.isWhite());
+
+
 
 module.exports = Unicorn;
