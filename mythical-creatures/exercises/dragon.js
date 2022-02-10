4896f1a8eb920c67db9762256a2a7886a1850466
Nikhila Balmoori <nbalmoori@gmail.com>
2022-02-09 18:22:53 -0700

    Complete dragon test

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,1 +1,23 @@
 
+
+class Dragon {
+  constructor (name, rider, timesEaten, ishungry) {
+    this.name = name;
+    this.rider = rider;
+    this.timesEaten = timesEaten || 0;
+    this.hungry = true;
+    }
+
+  greet() {
+    return `Hi, ${this.rider}!`
+  };
+
+  eat() {
+    this.timesEaten += 1
+    if (this.timesEaten >= 3) {
+      return this.hungry = false
+    }
+  }
+};
+
+module.exports = Dragon;
