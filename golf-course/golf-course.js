91738916907b0dd973a3bbeaaf02c4f6aa6ce84f
Nikhila Balmoori <nbalmoori@gmail.com>
2022-02-15 22:14:02 -0700

    Complete golf course test

--- golf-course/golf-course.js
+++ golf-course/golf-course.js
@@ -1,6 +1,22 @@
 class GolfCourse {
-
-
-}
+  constructor(name, difficulty, openings, features) {
+    this.name = name;
+    this.difficulty = difficulty;
+    this.openings = openings;
+    this.features = features;
+    this.currentlyPlaying = [];
+  }
+  checkInGroup(group) {
+    if (this.openings < group.length){
+      return `Sorry, we are currently booked! Please come back later.`
+    } else {
+    for (var i = 0; i < group.length; i++) {
+      this.currentlyPlaying.unshift(group[i].name)
+    }
+    this.openings -= group.length
+    return `You're checked in. Have fun!`
+    }
+  }
+};
 
 module.exports = GolfCourse;
--- golf-course/golfer.js
+++ golf-course/golfer.js
@@ -1,6 +1,41 @@
 class Golfer {
+  constructor (golfer) {
+    this.name = golfer.name;
+    this.handicap = golfer.handicap;
+    this.frustration = 0;
+    this.confidence = 0;
+  }
+  calculateAvg(par) {
+    var average = par + this.handicap
+    return `I usually shoot a ${average} on average.`
+  }
+  playRound(course) {
+    if (course.difficulty === 'hard'){
+      this.frustration += 500
+    } else if (course.difficulty === 'moderate'){
+      this.frustration += 100
+    }
+  }
+  hitTheRange() {
+    this.confidence += 10
+  }
+  marvel(course) {
+    return `I love the ${course.features[0]} and ${course.features[1]} on this course!`
+  }
+  whatYaShoot(shots){
+    if (shots >0) {
+      this.frustration += 20
+      return 'Doh!'
+    } else if (shots < 0) {
+      this.frustration = 0
+      return 'I AM THE GREATEST GOLFER ALIVE!'
+    } else {
+      this.frustration -= 10
+      return 'Booyah!'
+    }
+  }
+};
 
 
-}
 
 module.exports = Golfer;
