bd644d3bc0736150143f83adc396eec5bb2c1591
Nikhila Balmoori <nbalmoori@gmail.com>
2022-02-15 21:04:08 -0700

    Complete roadrace test

--- roadrace/src/roadrace.js
+++ roadrace/src/roadrace.js
@@ -1,5 +1,22 @@
 class Roadrace {
-
-}
+  constructor(roadraceObject) {
+    this.name = roadraceObject.title;
+    this.location = roadraceObject.city;
+    this.participants = [];
+  }
+  setDistance(miles) {
+    this.distance = miles
+    return `The ${this.name} in ${this.location} is a ${this.distance} mile race.`
+  }
+  registerParticipants(participant) {
+    this.participants.push(participant)
+  }
+  completeRace(){
+    for (var i = 0; i < this.participants.length; i++) {
+      this.participants[i].completedRaces.push(this.name)
+      this.participants[i].milesRun =+ this.distance
+    }
+  }
+};
 
 module.exports = Roadrace;
