c5c69df26adb78642de43db510de468201f98401
Nikhila Balmoori <nbalmoori@gmail.com>
2022-02-15 20:23:05 -0700

    Complete material test

--- crafting/src/craft.js
+++ crafting/src/craft.js
@@ -1,5 +1,19 @@
 class Craft {
-
+  constructor(craftObject) {
+    this.name = craftObject.type;
+    this.materials = craftObject.materials;
+    this.completed = false;
+  }
+  completeCraft() {
+    this.completed = true;
+    return `All done! It looks great!`
+  }
+  calculateProjectTotal() {
+    var projectTotal = 0
+    for (var i = 0; i < this.materials.length; i++) {
+    projectTotal += this.materials[i].calculateMaterialCost()}
+    return projectTotal
+  }
 }
 
 module.exports = Craft;
