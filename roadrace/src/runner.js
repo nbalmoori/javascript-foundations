d88f643e43e3f1e79276650c712e4bfa4e123e7e
Nikhila Balmoori <nbalmoori@gmail.com>
2022-02-15 20:41:19 -0700

    Complete runner test

--- roadrace/src/runner.js
+++ roadrace/src/runner.js
@@ -1,5 +1,22 @@
 class Runner {
-
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+    this.milesRun = 0;
+    this.fitness = 25;
+    this.completedRaces = [];
+  }
+  runSomeMiles(milesRan) {
+    this.milesRun += milesRan;
+    this.fitness += milesRan;
+  }
+  stretch() {
+    this.fitness += .5
+  }
+  runRace(race, raceMiles){
+    this.completedRaces.push(race)
+    this.runSomeMiles(raceMiles)
+  }
 }
 
 module.exports = Runner;
