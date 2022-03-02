dbfdbfb3efcbc9227a56bdd91f23d1d0696f134d
Nikhila Balmoori <nbalmoori@gmail.com>
2022-03-01 22:08:30 -0700

    Complete machine test

--- vending-machine/src/vending-machine.js
+++ vending-machine/src/vending-machine.js
@@ -1,5 +1,33 @@
 class VendingMachine {
-
+  constructor(vendingMachine) {
+    this.id = vendingMachine.id;
+    this.isBroken = vendingMachine.isBroken;
+    this.snacks = [];
+  }
+  addSnacks(snack) {
+    for (var i = 0; i < this.snacks.length; i++) {
+      if (this.snacks[i].name === snack.name) {
+        var snackExists = true
+      }
+    }
+    if (snackExists) {
+      return `Sorry, that snack is already stocked! Try adding a different snack.`
+    } else {
+      this.snacks.push(snack);
+    }
+  }
+  purchaseSnack(snackName, cash) {
+    for (var i = 0; i < this.snacks.length; i++) {
+      if (this.snacks[i].name === snackName && this.snacks[i].itemsInStock > 0 && cash >= this.snacks[i].price) {
+        this.snacks[i].itemsInStock -= 1;
+        return `Success! Here is $${cash - this.snacks[i].price} back!`
+      } else if (this.snacks[i].itemsInStock === 0) {
+        return `Sorry, no items in stock. Try another item.`
+      } else if (this.snacks[i].price > cash) {
+        return `Sorry, not enough payment. Please add more money.`
+      }
+    }
+  }
 }
 
 module.exports = VendingMachine;
